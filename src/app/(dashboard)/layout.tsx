760f0089c4470f6d68ef3ae6ab47a2d4a3b5ac4b
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-10-17 21:11:23 +0300

    Refactor layout component to include Menu and Navbar components

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,14 +10,22 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        <Link href="/" />
-        <Image src={"/logo.png"} alt="logo" height={80} width={80} />
-        <span>SchoolLama</span>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src={"/logo.png"} alt="logo" height={32} width={32} />
+          <span className="hidden lg:block">SchoolLama</span>
+        </Link>
+        <Menu />
       </div>
       {/* RIGHT */}
 
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200"></div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
